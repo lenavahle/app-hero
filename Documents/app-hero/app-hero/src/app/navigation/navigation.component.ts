010a579176d3fb47964e12561ed0369b1e7a59d4
Lena Vahle <lena@Lenas-MBP-3.fritz.box>
2019-11-16 02:57:48 +0100

    continues

--- Documents/app-hero/app-hero/src/app/navigation/navigation.component.ts
+++ Documents/app-hero/app-hero/src/app/navigation/navigation.component.ts
@@ -1,20 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { texte } from 'src/texte';
+import { tap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent implements OnInit {
 
+
+export class NavigationComponent implements OnInit {
+  public imagesUrl;
+
+  public text = texte;
+  
+  hover = false;
+ 
   constructor() { }
 
   ngOnInit() {
+    this.imagesUrl = [
+      '../assets/images/IMG_4575.JPG',
+      '../assets/images/IMG_4565.JPG',
+      '../assets/images/IMG_4573.JPG',
+      '../assets/images/IMG_4571.JPG',
+      '../assets/images/IMG_4480.JPG',
+      '../assets/images/IMG_4459.JPG',
+    ]
+    console.log(event)
+
   }
 
   scroll(value: string) {
     const element = document.querySelector('#' + value);
     element.scrollIntoView({behavior: 'smooth', block: 'start'});
+  }
+
+  public test() {
+    console.log('st')
   }
 
 }
---
+++ Documents/app-hero/app-hero/src/texte.ts
@@ -0,0 +1,3 @@
+export const texte = {
+    subtitle: 'Oekologischer Landbau aus Leidenschaft'
+}
